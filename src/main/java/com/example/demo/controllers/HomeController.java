fe4412095c5f1903c62230c99b9ee69e60bd1cf2
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-16 12:02:30 +0200

    add environment files

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Value("${message}")
+    private String message;
+
     @GetMapping({"/", "/home"})
     public String home(Model model) {
+
+        model.addAttribute("message", message);
         return "index";
     }
 
